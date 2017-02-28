b1805fa7f976368d62333bd7edc9dd248b27e89e
DELL <DELL@DELL-PC>
2017-02-28 14:44:51 +0530

    adding the hashCode() and equals() method

--- src/com/app/Vendor.java
+++ src/com/app/Vendor.java
@@ -39,11 +39,43 @@ public class Vendor {
 		this.venName = venName;
 	}
 	public Vendor(int venId, String venName, double venSal) {
-		super();
+		
 		this.venId = venId;
 		this.venName = venName;
 		this.venSal = venSal;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + venId;
+		result = prime * result + ((venName == null) ? 0 : venName.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(venSal);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendor other = (Vendor) obj;
+		if (venId != other.venId)
+			return false;
+		if (venName == null) {
+			if (other.venName != null)
+				return false;
+		} else if (!venName.equals(other.venName))
+			return false;
+		if (Double.doubleToLongBits(venSal) != Double.doubleToLongBits(other.venSal))
+			return false;
+		return true;
+	}
+	
 	
 	
 	
